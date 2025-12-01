64b9439740758f53269b7e9210f7b84ca668b10e
Muhammad Soffatur Rohman <soffatur26@gmail.com>
2025-12-01 11:12:07 +0700

    add 1 portfolio dlk

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -5,6 +5,24 @@ export default function Portfolio() {
     const status = 'pass'
     const datas = [
         {
+            client_project: "Business Trip Management System (DLK)",
+            client_name: "Internal",
+            desc: "Aplikasi web untuk keperluan dinas luar kota, terdapat role User, Verifikator, Kepala divisi, HRGA, FAA & Direktur.",
+            role: "Fullstack Developer & System Analyst (Stakeholder Communication & DevOps Responsibilities)",
+            company: "PT Arga Fasad Indonesia",
+            status: "done",
+            contribution: [
+                'Melakukan analisis kebutuhan secara menyeluruh untuk memastikan aplikasi sesuai proses bisnis Dinas Dalam Kota.',
+                'Mengembangkan aplikasi secara fullstack, mencakup flow bisnis, implementasi frontend, dan backend ',
+                'Mengelola environment server menggunakan Docker, serta melakukan konfigurasi Nginx Reverse Proxy / Nginx Proxy Manager untuk stabilitas dan keamanan aplikasi.',
+                'Melakukan presentasi, sosialisasi, dan simulasi penggunaan aplikasi kepada beberapa divisi sebagai bagian dari implementasi sistem.',
+                'Berkoordinasi dengan stakeholder lintas divisi untuk memastikan fitur berjalan sesuai kebutuhan operasional.'
+            ],
+            tech: ["Laravel", "Bootstrap", "JQuery", "MySQL", "Nginx", "Docker", "Draw.io"],
+            result: "Berhasil merilis aplikasi Business Trip Management (DLK), sebuah sistem internal untuk pengajuan dan approval perjalanan dinas dengan role berjenjang (User, Verifikator, Kepala Divisi, HRGA, FAA, dan Direktur). Implementasi fitur perhitungan jarak berbasis Maps dan otomatisasi perhitungan biaya berhasil meningkatkan akurasi data dan efisiensi proses perizinan. Sistem ini kini digunakan oleh beberapa divisi sebagai bagian dari operasional harian perusahaan",
+            result_en: "Successfully launched the Business Trip Management (DLK) application, an internal system for submitting and approving business trips with tiered roles (User, Verifier, Division Head, HRGA, FAA, and Director). The implementation of a map-based distance calculation feature and automated cost calculations has significantly improved data accuracy and the efficiency of the permitting process. This system is now used by several divisions as part of the company's daily operations."
+        },
+        {
             client_project: "Web Company Profile",
             client_name: "Internal",
             desc: "Web company profile berbasi web & CMS untuk meng custom dari informasi data sampai template",
